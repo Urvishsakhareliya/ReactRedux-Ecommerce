3b4dfc7ff3d01c5e75a8a9a52b834aaca03b627d
Urvishsakhareliya <106795173+Urvishsakhareliya@users.noreply.github.com>
2023-08-17 09:14:01 +0530

    16/08/2023

---
+++ src/component/OurTopCategories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function OurTopCategories() {
+  return (
+    <div>
+      <h1>Our Top Categories</h1>
+    </div>
+  );
+}
+
+export default OurTopCategories;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,12 @@
 import React from "react";
 import MainBanner from "../component/MainBanner";
+import OurTopCategories from "../component/OurTopCategories";
 
 export default function Home() {
-  return <MainBanner />;
+  return (
+    <>
+      <MainBanner />
+      <OurTopCategories />
+    </>
+  );
 }
