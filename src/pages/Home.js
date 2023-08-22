5496d413a603507b6135a41963114f3bd6e67036
Urvishsakhareliya <106795173+Urvishsakhareliya@users.noreply.github.com>
2023-08-22 16:24:08 +0530

    22/08/2023

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,11 @@ import {
   Loading,
   OurFeatureProduct,
 } from "../redux/action/categories";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { RiInstagramFill } from "react-icons/ri";
+import { SiYoutubemusic } from "react-icons/si";
+import { BsFacebook } from "react-icons/bs";
+import { TfiLinkedin } from "react-icons/tfi";
 
 export default function Home() {
   const Api = "https://api.pujakaitem.com/api/products";
@@ -46,7 +50,39 @@ export default function Home() {
         </div>
       </Link>
 
-      <h1 className="mb50">margin</h1>
+      {/* Talk to us */}
+
+      <section className="containerX text-center TalkToUs sec-margin">
+        <h2 className="colorHeading">Talk To Us!!</h2>
+        <NavLink className="makeInquiry mt-4 d-inline-block">
+          Make an Inquiry
+        </NavLink>
+      </section>
+
+      <footer className="sec-margin FooterSec">
+        <div className="containerX">
+          <h2 className="colorPrimary text-center fw-bolder">TORDO</h2>
+          <div className="socialLink colorPrimary text-center mt-4">
+            <p className="socialTEXT">Social links</p>
+            <div className="d-flex align-items-center justify-content-center gap-3 fs-1">
+              <Link>
+                <BsFacebook />
+              </Link>
+              <Link>
+                <RiInstagramFill />
+              </Link>
+              <Link>
+                <SiYoutubemusic />
+              </Link>
+              <Link>
+                <TfiLinkedin />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+      {/* <h1 className="mb50">margin</h1> */}
     </>
   );
 }
